388b0ef561f41e01c47fc88eae284926bba9b7d5
Eito Kochi <kochi.8@outlook.jp>
2023-11-20 08:53:49 +0900

    [Chnage] noopenerからnoreferrerに変更

--- src/components/ExhibitionDetail/Header.js
+++ src/components/ExhibitionDetail/Header.js
@@ -4,11 +4,11 @@ export default function Header() {
     return (
         <header className="archive">
             <div className="archiveLink">
-                <a href="https://www.tamabi.ac.jp/integrated/exhibit/graduation_19/" target="_blank" rel="noopener">2019</a>
-                <a href="https://www.tamabi.ac.jp/integrated/exhibit/graduation_20/" target="_blank" rel="noopener">2020</a>
-                <a href="https://www.tamabi.ac.jp/integrated/exhibit/graduation_21/" target="_blank" rel="noopener">2021</a>
-                <a href="https://www.tamabi.ac.jp/integrated/exhibit/graduation_22/" target="_blank" rel="noopener">2022</a>
-                <a href="https://integrated.jp/gw23/" target="_blank" rel="noopener">2023</a>
+                <a href="https://www.tamabi.ac.jp/integrated/exhibit/graduation_19/" target="_blank" rel="noreferrer">2019</a>
+                <a href="https://www.tamabi.ac.jp/integrated/exhibit/graduation_20/" target="_blank" rel="noreferrer">2020</a>
+                <a href="https://www.tamabi.ac.jp/integrated/exhibit/graduation_21/" target="_blank" rel="noreferrer">2021</a>
+                <a href="https://www.tamabi.ac.jp/integrated/exhibit/graduation_22/" target="_blank" rel="noreferrer">2022</a>
+                <a href="https://integrated.jp/gw23/" target="_blank" rel="noreferrer">2023</a>
             </div>
             <span>ARCHIVE</span>
         </header>
