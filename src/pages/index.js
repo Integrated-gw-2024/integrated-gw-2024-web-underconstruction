e322c68fb4c940e97ff9096e9f460ee8cec0d23c
Eito Kochi <kochi.8@outlook.jp>
2023-11-20 05:05:53 +0900

    [Change] 読み込むフォントを減らした

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,20 +82,12 @@ export default Index;
 export function Head() {
     return (
         <>
-            <script>
+            <link rel="stylesheet" href={`https://use.typekit.net/${process.env.ADOBE_FONTS_PROJECT_ID}.css`} />
+            <style>
                 {`
-                    {
-                        (function(d) {
-                            var config = {
-                                kitId: 'qsq6yov',
-                                scriptTimeout: 3000,
-                                async: true
-                            },
-                            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-                        })(document);
-                    }
+                body {font-family: "tt-commons-pro", "游ゴシック体", YuGothic, "游ゴシック", "Yu Gothic", sans-serif;}
                 `}
-            </script>
+            </style>
             <SEO />
         </>
     );
